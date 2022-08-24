ae445c7f0586aa43253c891ca2860c1f49b76106
pzcn <sdustpedro@gmail.com>
2022-08-24 09:17:35 +0800

    Commit from TeleportHQ

---
+++ src/components/gallery-card1.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="gallery-card1-gallery-card" v-bind:class="rootClassName">
+    <img :alt="image_alt" :src="image_src" class="gallery-card1-image" />
+    <h2 class="gallery-card1-text">{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GalleryCard1',
+  props: {
+    title: {
+      type: String,
+      default: 'Project Title',
+    },
+    image_alt: {
+      type: String,
+      default: 'image',
+    },
+    rootClassName: String,
+    image_src: {
+      type: String,
+      default:
+        'https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDEyfHxmb3Jlc3R8ZW58MHx8fHwxNjI2MjUxMjg4&ixlib=rb-1.2.1&w=1500',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.gallery-card1-gallery-card {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+.gallery-card1-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.gallery-card1-text {
+  align-self: center;
+  margin-top: var(--dl-space-space-halfunit);
+  font-weight: 600;
+  margin-bottom: var(--dl-space-space-halfunit);
+}
+
+
+
+@media(max-width: 991px) {
+  .gallery-card1-gallery-card {
+    width: auto;
+  }
+  .gallery-card1-image {
+    width: 300px;
+    height: 300px;
+  }
+  .gallery-card1-root-class-name {
+    width: auto;
+  }
+  .gallery-card1-root-class-name1 {
+    width: auto;
+  }
+  .gallery-card1-root-class-name2 {
+    width: auto;
+  }
+}
+@media(max-width: 767px) {
+  .gallery-card1-gallery-card {
+    width: auto;
+    flex-direction: column;
+  }
+  .gallery-card1-image {
+    width: 250px;
+    height: 250px;
+  }
+}
+@media(max-width: 479px) {
+  .gallery-card1-gallery-card {
+    width: auto;
+  }
+  .gallery-card1-image {
+    width: 150px;
+    height: 150px;
+  }
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,605 @@
+<template>
+  <div class="home-container">
+    <div class="home-banner">
+      <h1 class="home-text">865续命计划</h1>
+      <span class="home-text01">为小米865系列机型继续提供开发板内测般的体验</span>
+    </div>
+    <div class="home-banner1"><h1>获取ROM</h1></div>
+    <div class="home-blog">
+      <div class="home-container1">
+        <a
+          href="https://www.123pan.com/s/jKv8Vv-SiDWd"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="home-link"
+        >
+          <blog-post-card11
+            au2="@柚稚的孩纸"
+            date="更新时间：2022.08.22 19:31"
+            md521="umiMD5"
+            time2="umiTIME"
+            title="小米10"
+            button="下载"
+            pkgname2="umiPKG"
+            image_src="/playground_assets/umi-400h.png"
+            maintainer="维护者：@柚稚的孩纸"
+            rootClassName="rootClassName3"
+            class="home-component"
+          ></blog-post-card11>
+        </a>
+      </div>
+      <div class="home-container2">
+        <a
+          href="https://www.123pan.com/s/jKv8Vv-liDWd"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="home-link1"
+        >
+          <blog-post-card11
+            au2="@柚稚的孩纸"
+            date="更新时间：cmiTIME"
+            md521="cmiMD5"
+            time2="cmiTIME"
+            title="小米10 Pro"
+            button="下载"
+            pkgname2="cmiPKG"
+            image_src="/playground_assets/cmi-400h.png"
+            maintainer="维护者：@柚稚的孩纸"
+            rootClassName="rootClassName2"
+            class="home-component1"
+          ></blog-post-card11>
+        </a>
+      </div>
+      <div class="home-container3">
+        <a
+          href="https://www.123pan.com/s/vLXDVv-aZIp3"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="home-link2"
+        >
+          <blog-post-card11
+            au2="@相见即是缘"
+            date="更新时间：lmiTIME"
+            md521="lmiMD5"
+            time2="lmiTIME"
+            title="红米K30 Pro"
+            button="下载"
+            pkgname2="lmiPKG"
+            image_src="/playground_assets/lmi-400h.png"
+            maintainer="维护者：@相见即是缘"
+            rootClassName="rootClassName1"
+            class="home-component2"
+          ></blog-post-card11>
+        </a>
+      </div>
+    </div>
+    <div class="home-banner2"><h1>获取更新通知</h1></div>
+    <div class="home-gallery">
+      <gallery-card1
+        title="小米10"
+        subtitle="包名：umiPKG"
+        image_src="https://wxpusher.zjiecode.com/api/qrcode/Qbod38VNGuC5RzILvXd6CkYx6cdKPSK43b6WfWh0qimpjBUVgXBZFxmaXtzEZqnE.jpg"
+        subtitle1="维护者：@柚稚的孩纸"
+        subtitle2="md5：umiMD5"
+        subtitle21="更新时间：umiTIME"
+        rootClassName="rootClassName"
+      ></gallery-card1>
+      <gallery-card1
+        title="小米10 Pro"
+        subtitle="包名：cmiPKG"
+        image_src="https://wxpusher.zjiecode.com/api/qrcode/eBmGQOVOrj0n9Fnan4VdZt0I1QGlDWiP2BdojXPpEUwevN7Ncjpey3EfvIAyBRHj.jpg"
+        subtitle1="维护者：@柚稚的孩纸"
+        subtitle2="md5：cmiMD5"
+        subtitle21="更新时间：cmiTIME"
+        rootClassName="rootClassName1"
+      ></gallery-card1>
+      <gallery-card1
+        title="红米K30 Pro"
+        subtitle="包名：lmiPKG"
+        image_src="https://wxpusher.zjiecode.com/api/qrcode/29T0NorxWMKiM8o6CJOcpQDWyzksZ8Fx9V8RHJpriZgYg8NHmsXOWLzUYlMJDZmQ.jpg"
+        subtitle1="维护者：@相见即是缘"
+        subtitle2="md5：lmiMD5"
+        subtitle21="更新时间：lmiTIME"
+        rootClassName="rootClassName2"
+      ></gallery-card1>
+    </div>
+    <div class="home-container4">
+      <span class="home-text04">
+        微信扫描上方二维码可订阅更新通知，长按可保存图片
+      </span>
+    </div>
+    <div class="home-banner3"><h1>注意事项</h1></div>
+    <div class="home-container5">
+      <span class="home-text05">
+        <span>1. 感谢酷安</span>
+        <a
+          href="https://www.coolapk.com/u/1614257"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="home-link3"
+        >
+          @相见即是缘
+        </a>
+        <span>、酷安</span>
+        <a
+          href="https://www.coolapk.com/u/1157881"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="home-link4"
+        >
+          @PedroZ
+        </a>
+        <span>对本包的大力支持，本包配合他们的</span>
+        <a
+          href="https://www.coolapk.com/feed/38510396"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="home-link5"
+        >
+          平板样式音量条
+        </a>
+        <span>和</span>
+        <a
+          href="https://www.coolapk.com/apk/dev.miuiicons.pedroz"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="home-link6"
+        >
+          完美图标计划
+        </a>
+        <span>效果更佳哦（我不允许你们还不知道哦）</span>
+        <br />
+        <br />
+        <span>
+          2.
+          本包基于小米12X，有酷友和组织内人员说有双5G等功能。请备份好您的数据，本包可以从官方版本直升，也可以从基于10S的包直升，但仍建议您备份数据以备不时之需。如您在官改，务必执行第三条
+        </span>
+        <br />
+        <br />
+        <span>
+          3.
+          如遇到无法开机，请您线刷官方包后再刷入（因您不遵守第2条导致的数据丢失请自负后果）。第一次使用本包建议双清，第二次使用可保留数据升级
+        </span>
+        <br />
+        <br />
+        <span>
+          4.
+          如果无法卡刷，请关闭签名验证。本包已删除系统更新，以防止一些不必要的提问
+        </span>
+        <br />
+        <br />
+        <span>
+          5. 本包已集成Magisk 25.2，如遇到Magisk无法工作，请您自行下载Magisk
+          25.2的apk安装。受限于别人的智力成果，本包不集成第三方Recovery，请您卡刷完后不要忘记相关操作来保留第三方Recovery（考虑到你已经开始刷第三方包了，还得用到第三方Recovery，我认为Magisk是必备品，如您觉得不需要可以去Magisk卸载）
+        </span>
+        <br />
+        <br />
+        <span>
+          6. 升级后可能影响data解密，可尝试取消锁屏密码解锁，介意请勿刷入
+        </span>
+        <br />
+        <br />
+        <span>7. 指纹快捷功能容易误触（这个可能是MIUI的Bug），暂无其他bug</span>
+        <br />
+        <br />
+        <span>
+          8.
+          移植包请您不要留言询问模块相关事项，有得用就不错了，你总得在某些地方做取舍，你说对吗？
+        </span>
+        <br />
+        <br />
+        <span>
+          9.
+          如果您遇到问题，请持续观望是否是小米12X有问题撤包，你也可以加群反馈，我会及时处理
+        </span>
+        <br />
+        <br />
+        <span>
+          10. 本包已删除可在商店下载的推广，如您需要请前往应用商店自行下载
+        </span>
+        <br />
+        <br />
+        <span>
+          11.
+          如您问一些大家公认的无意义的问题（如如何刷，如何使用的），我会参照12条处理
+        </span>
+        <br />
+        <br />
+        <span>
+          12.
+          对于不看本升级说明的，如有提问，从2022年8月15日开始，我会回复相关条数并将你拉黑
+        </span>
+        <br />
+        <br />
+        <span>
+          13. 组织地址：791915028
+          <span v-html="rawll8w"></span>
+        </span>
+        <a
+          href="https://qm.qq.com/cgi-bin/qm/qr?k=dAJdeImIx8Yb21wSFRynt06TzDe0tzcW&amp;jump_from=webapi"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="home-link7"
+        >
+          [点击加入]
+        </a>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import BlogPostCard11 from '../components/blog-post-card11'
+import GalleryCard1 from '../components/gallery-card1'
+
+export default {
+  name: 'Home',
+  components: {
+    BlogPostCard11,
+    GalleryCard1,
+  },
+
+  data() {
+    return {
+      rawll8w: ' ',
+    }
+  },
+
+  metaInfo: {
+    title: '865续命计划',
+    meta: [
+      {
+        property: 'og:title',
+        content: '865续命计划',
+      },
+    ],
+  },
+}
+</script>
+
+<style scoped>
+.home-container {
+  width: 100%;
+  display: flex;
+  overflow: auto;
+  min-height: 100vh;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.home-banner {
+  width: 100%;
+  display: flex;
+  padding: var(--dl-space-space-threeunits);
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.home-text {
+  font-size: 5rem;
+  text-align: center;
+}
+.home-text01 {
+  font-size: 20px;
+  max-width: var(--dl-size-size-maxwidth);
+  margin-top: var(--dl-space-space-twounits);
+  text-align: center;
+  margin-bottom: var(--dl-space-space-twounits);
+}
+.home-banner1 {
+  width: 100%;
+  display: flex;
+  padding: var(--dl-space-space-threeunits);
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.home-blog {
+  width: 100%;
+  display: flex;
+  padding: var(--dl-space-space-threeunits);
+  max-width: var(--dl-size-size-maxwidth);
+  align-self: center;
+  align-items: center;
+  justify-content: space-between;
+}
+.home-container1 {
+  display: flex;
+  align-items: center;
+  margin-right: var(--dl-space-space-twounits);
+  justify-content: space-between;
+}
+.home-link {
+  display: contents;
+}
+.home-component {
+  text-decoration: none;
+}
+.home-container2 {
+  display: flex;
+  align-items: center;
+  margin-right: var(--dl-space-space-twounits);
+  justify-content: space-between;
+}
+.home-link1 {
+  display: contents;
+}
+.home-component1 {
+  text-decoration: none;
+}
+.home-container3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.home-link2 {
+  display: contents;
+}
+.home-component2 {
+  text-decoration: none;
+}
+.home-banner2 {
+  width: 100%;
+  display: flex;
+  padding: var(--dl-space-space-threeunits);
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.home-gallery {
+  width: 100%;
+  display: grid;
+  padding: var(--dl-space-space-oneandhalfunits);
+  grid-gap: var(--dl-space-space-unit);
+  max-width: var(--dl-size-size-maxwidth);
+  align-self: center;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+.home-container4 {
+  flex: 0 0 auto;
+  width: auto;
+  height: auto;
+  display: flex;
+  align-self: center;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+.home-text04 {
+  font-size: 28px;
+}
+.home-banner3 {
+  width: 100%;
+  display: flex;
+  padding: var(--dl-space-space-threeunits);
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.home-container5 {
+  flex: 0 0 auto;
+  width: auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.home-text05 {
+  padding-left: 256px;
+  padding-right: 256px;
+}
+@media(max-width: 1600px) {
+  .home-text {
+    font-size: 5rem;
+  }
+  .home-text01 {
+    font-size: 20px;
+  }
+  .home-blog {
+    align-self: center;
+  }
+  .home-container4 {
+    width: auto;
+    align-self: center;
+  }
+  .home-text04 {
+    font-size: 28px;
+  }
+  .home-container5 {
+    width: auto;
+    align-self: center;
+  }
+  .home-text05 {
+    padding-left: 128px;
+    padding-right: 128px;
+  }
+}
+@media(max-width: 1200px) {
+  .home-text {
+    font-size: 5rem;
+  }
+  .home-text01 {
+    font-size: 20px;
+  }
+  .home-container4 {
+    width: auto;
+    height: auto;
+    align-self: center;
+  }
+  .home-text04 {
+    width: auto;
+    font-size: 28px;
+  }
+  .home-container5 {
+    width: auto;
+    align-self: center;
+  }
+  .home-text05 {
+    font-size: 22px;
+    margin-left: var(--dl-space-space-threeunits);
+    margin-right: var(--dl-space-space-threeunits);
+    padding-left: var(--dl-space-space-halfunit);
+    padding-right: var(--dl-space-space-halfunit);
+  }
+}
+@media(max-width: 991px) {
+  .home-text {
+    font-size: 4rem;
+  }
+  .home-text01 {
+    font-size: 18px;
+  }
+  .home-blog {
+    flex-direction: column;
+  }
+  .home-container1 {
+    margin-right: 0px;
+    margin-bottom: var(--dl-space-space-threeunits);
+  }
+  .home-container2 {
+    margin-right: 0px;
+    margin-bottom: var(--dl-space-space-threeunits);
+  }
+  .home-gallery {
+    grid-template-columns: 1fr 1fr;
+  }
+  .home-container4 {
+    width: auto;
+    height: auto;
+    align-self: center;
+  }
+  .home-text04 {
+    font-size: 24px;
+  }
+  .home-text05 {
+    padding-left: var(--dl-space-space-halfunit);
+    padding-right: var(--dl-space-space-halfunit);
+  }
+}
+@media(max-width: 767px) {
+  .home-banner {
+    padding-left: var(--dl-space-space-twounits);
+    padding-right: var(--dl-space-space-twounits);
+  }
+  .home-text {
+    font-size: 4rem;
+  }
+  .home-text01 {
+    font-size: 18px;
+    padding-left: var(--dl-space-space-unit);
+    padding-right: var(--dl-space-space-unit);
+  }
+  .home-banner1 {
+    padding-left: var(--dl-space-space-twounits);
+    padding-right: var(--dl-space-space-twounits);
+  }
+  .home-blog {
+    padding-left: var(--dl-space-space-twounits);
+    padding-right: var(--dl-space-space-twounits);
+  }
+  .home-banner2 {
+    padding-left: var(--dl-space-space-twounits);
+    padding-right: var(--dl-space-space-twounits);
+  }
+  .home-gallery {
+    padding-left: var(--dl-space-space-twounits);
+    padding-right: var(--dl-space-space-twounits);
+  }
+  .home-container4 {
+    width: auto;
+    height: auto;
+    align-self: center;
+  }
+  .home-banner3 {
+    padding-left: var(--dl-space-space-twounits);
+    padding-right: var(--dl-space-space-twounits);
+  }
+  .home-text05 {
+    padding-left: var(--dl-space-space-halfunit);
+    padding-right: var(--dl-space-space-halfunit);
+  }
+}
+@media(max-width: 479px) {
+  .home-banner {
+    padding-top: var(--dl-space-space-twounits);
+    padding-left: var(--dl-space-space-unit);
+    padding-right: var(--dl-space-space-unit);
+    padding-bottom: var(--dl-space-space-twounits);
+  }
+  .home-text {
+    font-size: 3rem;
+  }
+  .home-text01 {
+    font-size: 14px;
+  }
+  .home-banner1 {
+    padding-top: var(--dl-space-space-twounits);
+    padding-left: var(--dl-space-space-unit);
+    padding-right: var(--dl-space-space-unit);
+    padding-bottom: var(--dl-space-space-twounits);
+  }
+  .home-blog {
+    padding-top: var(--dl-space-space-twounits);
+    padding-left: var(--dl-space-space-unit);
+    padding-right: var(--dl-space-space-unit);
+    padding-bottom: var(--dl-space-space-twounits);
+  }
+  .home-banner2 {
+    padding-top: var(--dl-space-space-twounits);
+    padding-left: var(--dl-space-space-unit);
+    padding-right: var(--dl-space-space-unit);
+    padding-bottom: var(--dl-space-space-twounits);
+  }
+  .home-gallery {
+    padding-top: var(--dl-space-space-halfunit);
+    padding-left: var(--dl-space-space-unit);
+    padding-right: var(--dl-space-space-unit);
+    padding-bottom: var(--dl-space-space-halfunit);
+  }
+  .home-container4 {
+    width: auto;
+    height: 28px;
+    align-self: center;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+  }
+  .home-text04 {
+    font-size: 14px;
+  }
+  .home-banner3 {
+    padding-top: var(--dl-space-space-twounits);
+    padding-left: var(--dl-space-space-unit);
+    padding-right: var(--dl-space-space-unit);
+    padding-bottom: var(--dl-space-space-twounits);
+  }
+  .home-container5 {
+    width: auto;
+    margin-right: 0px;
+    margin-bottom: var(--dl-space-space-threeunits);
+  }
+  .home-text05 {
+    width: auto;
+    font-size: 16px;
+    margin-left: 20px;
+    margin-right: 20px;
+    padding-left: var(--dl-space-space-halfunit);
+    padding-right: var(--dl-space-space-halfunit);
+  }
+  .home-link3 {
+    text-decoration: underline;
+  }
+  .home-link4 {
+    text-decoration: underline;
+  }
+  .home-link5 {
+    text-decoration: underline;
+  }
+  .home-link6 {
+    text-decoration: underline;
+  }
+  .home-link7 {
+    text-decoration: underline;
+  }
+}
+</style>
